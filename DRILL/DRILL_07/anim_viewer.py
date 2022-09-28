eeb1a8a19a82f09f6a23b6979aaf4dbf2dd98140
강성민 <106009675+Seeghoul@users.noreply.github.com>
2022-09-28 23:45:35 +0900

    test

--- DRILL/DRILL_07/anim_viewer.py
+++ DRILL/DRILL_07/anim_viewer.py
@@ -6,12 +6,28 @@ character = load_image('characters.gif')
 x = 0
 frame = 0
 
-for x in range (0, 800, 10):
+
+def plus():
+    frame = 0
+    for a in range(0,3):
+        clear_canvas()
+        character.clip_draw(frame * 20 + 320, 400, 20, 80, x, 90)
+        update_canvas()
+        frame = (frame + 1) % 3
+        delay(0.3)
+        get_events()
+
+def minus():
+    frame = 1
     clear_canvas()
-    character.clip_draw(frame * 20 + 320, 400, 20, 80, x, 90,)
+    character.clip_draw(frame * 20 + 320, 400, 20, 80, x, 90)
     update_canvas()
-    frame = (frame +1) % 3
-    delay(0.1)
+    delay(0.3)
     get_events()
+
+
+for x in range (0, 800, 4):
+    plus()
+    minus()
 
 close_canvas()
