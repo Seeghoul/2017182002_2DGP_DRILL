e5d6acd713a93b0de2d6643f8f733b88fe14675a
강성민 <106009675+Seeghoul@users.noreply.github.com>
2022-10-13 10:37:25 +0900

    project folder added

--- DRILL/DRILL_07/anim_viewer.py
+++ DRILL/DRILL_07/anim_viewer.py
@@ -11,18 +11,18 @@ def plus():
     frame = 0
     for a in range(0,3):
         clear_canvas()
-        character.clip_draw(frame * 20 + 320, 400, 20, 80, x, 90)
+        character.clip_draw(frame * 20 + 321, 400, 20, 80, x, 90)
         update_canvas()
         frame = (frame + 1) % 3
-        delay(0.3)
+        delay(0.1)
         get_events()
 
 def minus():
     frame = 1
     clear_canvas()
-    character.clip_draw(frame * 20 + 320, 400, 20, 80, x, 90)
+    character.clip_draw(frame * 20 + 321, 400, 20, 80, x, 90)
     update_canvas()
-    delay(0.3)
+    delay(0.1)
     get_events()
 
 
